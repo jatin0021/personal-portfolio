17ccb4e37be318b20e83e3770f2a3d610fc3367d
jatin0021 <jatinchauhan457@gmail.com>
2026-03-08 17:07:30 +0530

    server database updated

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // ─── Middleware ───────────────────────────────────────────────────────────────
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: "*",
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
   })
